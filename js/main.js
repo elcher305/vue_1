694a233044bbbde02d006660e4b48c45531eb798
Elvira <elvira.chernova.05@gmail.com>
2025-02-03 23:47:54 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -7,7 +7,8 @@ let app = new Vue({
         altText: "A pair of socks",
         //link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
         inStock: true,
-        //onSale: true // Управляет отображением элемента <span>
+        //onSale: true, // Управляет отображением элемента <span>
+        //sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],// Массив размеров
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
@@ -18,6 +19,7 @@ let app = new Vue({
                 variantId: 2235,
                 variantColor: 'blue'
             }
-        ]
+        ],
+        cart: 0
     }
 })
