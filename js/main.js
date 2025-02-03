83251678af9d162f63756146c476784fd0307fd9
Elvira <elvira.chernova.05@gmail.com>
2025-02-03 23:30:22 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -8,5 +8,16 @@ let app = new Vue({
         //link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
         inStock: true,
         //onSale: true // Управляет отображением элемента <span>
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ]
     }
 })
