e5d892bd329081e89c23e6fea107be5519d1b95b
Elvira <elvira.chernova.05@gmail.com>
2025-02-04 22:58:03 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,71 @@
+Vue.component('product-review', {
+    template: `
+
+<form class="review-form" @submit.prevent="onSubmit">
+
+<p v-if="errors.length">
+ <b>Please correct the following error(s):</b>
+ <ul>
+   <li v-for="error in errors">{{ error }}</li>
+ </ul>
+</p>
+
+ <p>
+   <label for="name">Name:</label>
+   <input id="name" v-model="name" placeholder="name">
+ </p>
+
+ <p>
+   <label for="review">Review:</label>
+   <textarea id="review" v-model="review"></textarea>
+ </p>
+
+ <p>
+   <label for="rating">Rating:</label>
+   <select id="rating" v-model.number="rating">
+     <option>5</option>
+     <option>4</option>
+     <option>3</option>
+     <option>2</option>
+     <option>1</option>
+   </select>
+ </p>
+
+ <p>
+   <input type="submit" value="Submit"> 
+ </p>
+
+</form>
+ `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            errors: []
+        }
+    },
+    methods:{
+        onSubmit() {
+            if(this.name && this.review && this.rating) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+                this.rating = null
+            } else {
+                if(!this.name) this.errors.push("Name required.")
+                if(!this.review) this.errors.push("Review required.")
+                if(!this.rating) this.errors.push("Rating required.")
+            }
+        }
+    }
+})
+
 Vue.component('product', {
     props: {
         premium: {
@@ -6,80 +74,54 @@ Vue.component('product', {
         }
     },
     template: `
-    <div class="product">
-        <div class="product-image">
-                <img :src="image" :alt="altText"/>
-            </div>
+   <div class="product">
+    <div class="product-image">
+           <img :src="image" :alt="altText"/>
+       </div>
 
-           
-            <div class="product-info">
-                <h1>{{ title }}</h1>
-                <p v-if="inStock">In stock</p>
-                <p v-else>Out of Stock</p>
-                <ul>
-                    <li v-for="detail in details">{{ detail }}</li>
-                </ul>
-                <p>Shipping: {{ shipping }}</p>
-                <div
-                        class="color-box"
-                        v-for="(variant, index) in variants"
-                        :key="variant.variantId"
-                        :style="{ backgroundColor:variant.variantColor }"
-                        @mouseover="updateProduct(index)"
-                ></div>
-
-                <p :class="{ 'out-of-stock': !inStock }">
-                        {{ inStock ? 'In Stock' : 'Out of Stock' }}
-                </p>
-
-                <div class="card">
-                    <span v-if="onSale" class="sale-text">On Sale</span>
-                    <h2>{{ brand }} {{ product }}</h2>
-                    <p>{{ description }}</p>
-                    <product-details :details="details"></product-details>
-                    <p :class="{ 'sale-message': onSale }">{{ sale }}</p>
-                </div>
-
-                <div class="size-product">
-                    <h2>Размеры носков:</h2>
-                    <a v-for="size in sizes" :key="size">
-                        {{ size }}
-                    </a>
-                </div>
-
-     
-                <button
-                        v-on:click="addToCart"
-                        :disabled="!inStock"
-                        :class="{ disabledButton: !inStock }"
-                >
-                    Add to cart
-                </button>
-        </div>
-        <h2>Корзина товаров</h2>
-        <div v-if="cart.length === 0">
-            <p>Ваша корзина пуста.</p>
-        </div>
-        
-        <div v-else>
-        <div class="cart-item" v-for="item in cart" :key="item.id">
-            <span>{{ item.name }} ({{ item.price }} руб.)</span>
-            <button class="remove-button" @click="removeFromCart(item.id)">Удалить</button>
-        </div>
-        </div>
-    </div>
-    `,
+       <div class="product-info">
+           <h1>{{ title }}</h1>
+           <p v-if="inStock">In stock</p>
+           <p v-else>Out of Stock</p>
+           <ul>
+               <li v-for="detail in details">{{ detail }}</li>
+           </ul>
+          <p>Shipping: {{ shipping }}</p>
+           <div
+                   class="color-box"
+                   v-for="(variant, index) in variants"
+                   :key="variant.variantId"
+                   :style="{ backgroundColor:variant.variantColor }"
+                   @mouseover="updateProduct(index)"
+           ></div>
+          
+           <button
+                   v-on:click="addToCart"
+                   :disabled="!inStock"
+                   :class="{ disabledButton: !inStock }"
+           >
+               Add to cart
+           </button>    
+       </div>           
+       <div>
+            <h2>Reviews</h2>
+            <p v-if="!reviews.length">There are no reviews yet.</p>
+            <ul>
+              <li v-for="review in reviews">
+              <p>{{ review.name }}</p>
+              <p>Rating: {{ review.rating }}</p>
+              <p>{{ review.review }}</p>
+              </li>
+            </ul>
+           </div> <product-review @review-submitted="addReview"></product-review>
+       </div>
+ `,
     data() {
         return {
             product: "Socks",
             brand: 'Vue Mastery',
-            description: 'A pair of warm, fuzzy socks',
             selectedVariant: 0,
             altText: "A pair of socks",
-            //link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-            inStock: true,
-            onSale: true, // Управляет отображением элемента <span>
-            sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],// Массив размеров
             details: ['80% cotton', '20% polyester', 'Gender-neutral'],
             variants: [
                 {
@@ -95,7 +137,7 @@ Vue.component('product', {
                     variantQuantity: 0
                 }
             ],
-            cart: 0
+            reviews: []
         }
     },
     methods: {
@@ -105,6 +147,9 @@ Vue.component('product', {
         updateProduct(index) {
             this.selectedVariant = index;
             console.log(index);
+        },
+        addReview(productReview) {
+            this.reviews.push(productReview)
         }
     },
     computed: {
@@ -114,7 +159,7 @@ Vue.component('product', {
         image() {
             return this.variants[this.selectedVariant].variantImage;
         },
-        inStock(){
+        inStock() {
             return this.variants[this.selectedVariant].variantQuantity
         },
         shipping() {
@@ -123,35 +168,18 @@ Vue.component('product', {
             } else {
                 return 2.99
             }
-        },
-        sale() {
-            if (this.onSale) {
-                return `Распродажа! Товар ${this.brand} ${this.product} со скидкой!`;
-            } else {
-                return `Распродажа на товар ${this.brand} ${this.product} не проводится.`;
-            }
         }
-
     }
 })
 let app = new Vue({
     el: '#app',
     data: {
         premium: true,
-        cart: [
-            { id: 1, name: 'Носки Nike', price: 500 },
-            { id: 2, name: 'Кроссовки Adidas', price: 4000 },
-            { id: 3, name: 'Футболка Puma', price: 1500 }
-        ]
+        cart: []
     },
     methods: {
         updateCart(id) {
             this.cart.push(id);
-        },
-        removeFromCart(itemId) {
-            // Удаляем товар из корзины по его идентификатору
-            this.cart = this.cart.filter(item => item.id !== itemId);
         }
     }
-
 })
