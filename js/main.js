e3ae590a8c96dbe8b661ff1709b585274f9718e4
Elvira <elvira.chernova.05@gmail.com>
2025-02-05 12:13:54 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -1,48 +1,49 @@
+let eventBus = new Vue()
+
+
 Vue.component('product-review', {
     template: `
-
-<form class="review-form" @submit.prevent="onSubmit">
-
-<p v-if="errors.length">
- <b>Please correct the following error(s):</b>
- <ul>
-   <li v-for="error in errors">{{ error }}</li>
- </ul>
-</p>
-
- <p>
-   <label for="name">Name:</label>
-   <input id="name" v-model="name" placeholder="name">
- </p>
-
- <p>
-   <label for="review">Review:</label>
-   <textarea id="review" v-model="review"></textarea>
- </p>
-
- <p>
-   <label for="rating">Rating:</label>
-   <select id="rating" v-model.number="rating">
-     <option>5</option>
-     <option>4</option>
-     <option>3</option>
-     <option>2</option>
-     <option>1</option>
-   </select>
- </p>
-
- <p>
-   <input type="submit" value="Submit"> 
- </p>
-
-</form>
+       <form class="review-form" @submit.prevent="onSubmit">
+       <p v-if="errors.length">
+             <b>Исправьте текущие ошибки:</b>
+             <ul>
+               <li v-for="error in errors">{{ error }}</li>
+             </ul>
+       </p>
+         <p>
+           <label for="name">Имя:</label>
+           <input id="name" v-model="name" placeholder="name">
+         </p>
+        
+         <p>
+           <label for="review">Отзыв:</label>
+           <textarea id="review" v-model="review"></textarea>
+         </p>
+         <p>
+           <label for="rating">Рейтинг:</label>
+           <select id="rating" v-model.number="rating">
+             <option>5</option>
+             <option>4</option>
+             <option>3</option>
+             <option>2</option>
+             <option>1</option>
+           </select>
+         </p>
+         
+         <p>
+           <input type="submit"> 
+         </p>
+        </form>
+        
+        
  `,
     data() {
         return {
             name: null,
             review: null,
             rating: null,
-            errors: []
+            errors: [],
+            recommended: null,
         }
     },
     methods:{
@@ -53,18 +54,21 @@ Vue.component('product-review', {
                     review: this.review,
                     rating: this.rating
                 }
-                this.$emit('review-submitted', productReview)
+                eventBus.$emit('review-submitted', productReview)
                 this.name = null
                 this.review = null
                 this.rating = null
             } else {
-                if(!this.name) this.errors.push("Name required.")
-                if(!this.review) this.errors.push("Review required.")
-                if(!this.rating) this.errors.push("Rating required.")
+                if(!this.name) this.errors.push("Имя обязательно.")
+                if(!this.review) this.errors.push("Отзыв обязателен.")
+                if(!this.rating) this.errors.push("Рейтинг обязателен.")
             }
-        }
+        },
     }
+
 })
+
+
 
 Vue.component('product', {
     props: {
@@ -81,48 +85,58 @@ Vue.component('product', {
 
        <div class="product-info">
            <h1>{{ title }}</h1>
-           <p v-if="inStock">In stock</p>
-           <p v-else>Out of Stock</p>
-           <ul>
-               <li v-for="detail in details">{{ detail }}</li>
-           </ul>
-          <p>Shipping: {{ shipping }}</p>
+           <p>{{description}}</p>
+           <span class="sale">{{ sale }}</span>
+           <p v-if="inStock" >В наличии</p>
+           <p v-else :class="{ OutOfStock:!inStock }">Нет в наличии</p>
+           <p v-if="errors.length">
+                <b>Исправьте текущие ошибки:</b>
+                <ul>
+                    <li v-for="error in errors">{{ error }}</li>
+                </ul>
+           </p>
            <div
                    class="color-box"
                    v-for="(variant, index) in variants"
                    :key="variant.variantId"
                    :style="{ backgroundColor:variant.variantColor }"
-                   @mouseover="updateProduct(index)"
+                   @click="updateProduct(index)"
            ></div>
-          
+                <div class="size-product">
+                    <a value="">Выберите размер</a>
+                    <div class="size">
+                        <a v-for="size in sizes" :value="size">{{size}}</a>
+                    </div>
+                </div>
+                
            <button
                    v-on:click="addToCart"
                    :disabled="!inStock"
                    :class="{ disabledButton: !inStock }"
            >
-               Add to cart
-           </button>    
+               Добавить в корзину
+           </button>
+           <button
+                   v-on:click="removeFromCart"
+           >
+               Удалить из корзины
+           </button>
+           <br>
+           <a v-bind:href="link">Похожие товары >></a>    
        </div>           
-       <div>
-            <h2>Reviews</h2>
-            <p v-if="!reviews.length">There are no reviews yet.</p>
-            <ul>
-              <li v-for="review in reviews">
-              <p>{{ review.name }}</p>
-              <p>Rating: {{ review.rating }}</p>
-              <p>{{ review.review }}</p>
-              </li>
-            </ul>
-           </div> <product-review @review-submitted="addReview"></product-review>
+           <product-tabs :reviews="reviews"></product-tabs>
        </div>
  `,
     data() {
         return {
             product: "Socks",
             brand: 'Vue Mastery',
+            description: "Пара теплых носков",
             selectedVariant: 0,
-            altText: "A pair of socks",
-            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+            selectedSize: '',
+            altText: "Socks",
+            link: "https://www.ozon.ru/category/odezhda-obuv-i-aksessuary-7500/?text=%D0%BD%D0%BE%D1%81%D0%BA%D0%B8&clid=11468697-1",
+            onSale: true,
             variants: [
                 {
                     variantId: 2234,
@@ -134,22 +148,40 @@ Vue.component('product', {
                     variantId: 2235,
                     variantColor: 'blue',
                     variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-                    variantQuantity: 0
+                    variantQuantity: 20
                 }
             ],
-            reviews: []
+            sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+            reviews: [],
+            errors: []
         }
     },
     methods: {
         addToCart() {
-            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
+            this.errors = [];
+            if (this.selectedSize === '') {
+                this.errors.push("Выберите размер.");
+            }
+            if (this.selectedVariant === null) {
+                this.errors.push("Выберите цвет.");
+            }
+            if (this.errors.length === 0) {
+                this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
+            }
+        },
+        removeFromCart(index) {
+            this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId);
         },
         updateProduct(index) {
             this.selectedVariant = index;
-            console.log(index);
         },
         addReview(productReview) {
             this.reviews.push(productReview)
+        },
+        mounted() {
+            eventBus.$on('review-submitted', productReview => {
+                this.reviews.push(productReview)
+            })
         }
     },
     computed: {
@@ -162,24 +194,87 @@ Vue.component('product', {
         inStock() {
             return this.variants[this.selectedVariant].variantQuantity
         },
+        sale(){
+            return this.onSale ? (`${this.brand} ${this.product} Скидка!`) : (`${this.brand} ${this.product} без скидки.`);
+        },
         shipping() {
             if (this.premium) {
-                return "Free";
+                return "Бесплатно";
             } else {
-                return 2.99
+                return "190₽"
             }
         }
     }
 })
+
+Vue.component('product-tabs', {
+    template: `
+     <div>   
+       <ul>
+         <span class="tab"
+               :class="{ activeTab: selectedTab === tab }"
+               v-for="(tab, index) in tabs"
+               @click="selectedTab = tab"
+         >{{ tab }}</span>
+       </ul>
+       <div v-show="selectedTab === 'Отзывы'">
+         <p v-if="!reviews.length"> Сейчас нет отзывов.</p>
+         <ul>
+           <li v-for="review in reviews">
+           <p>{{ review.name }}</p>
+           <p>Rating: {{ review.rating }}</p>
+           <p>{{ review.review }}</p>
+           </li>
+         </ul>
+       </div> 
+       <div v-show="selectedTab === 'Оставить отзыв'">
+         <product-review></product-review>
+     </div>
+     <div v-show="selectedTab === 'Детали'">
+        <product-detail></product-detail>
+    </div>
+     </div>
+`,
+    props: {
+        reviews: {
+            type: Array,
+            required: false
+        }
+    },
+    data() {
+        return {
+            tabs: ['Отзывы', 'Оставить отзыв', 'Детали'],
+            selectedTab: 'Отзывы'
+        }
+    }
+})
+
+Vue.component('product-detail', {
+    template:`
+    <ul>
+        <li v-for="detail in details">{{ detail }}</li>
+    </ul>`,
+    data () {
+        return {
+            details: ['80% хлопок', '20% полиэстер', 'Унисекс'],
+        }
+    }
+})
+
 let app = new Vue({
     el: '#app',
     data: {
-        premium: true,
+        premium: false,
         cart: []
     },
     methods: {
         updateCart(id) {
             this.cart.push(id);
-        }
-    }
+        },
+        eraseCart(id) {
+            this.cart.pop(id);
+        },
+
+    },
 })
+
